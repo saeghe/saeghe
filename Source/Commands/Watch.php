5fc4dc838d9f038cb93e909e880c8d73e5dc62d4
Morteza <morteza@protonmail.com>
2022-11-26 08:14:15 +0100

    Fix watch command

--- Source/Commands/Watch.php
+++ Source/Commands/Watch.php
@@ -4,17 +4,15 @@ namespace Saeghe\Saeghe\Commands\Watch;
 
 use Saeghe\Saeghe\Project;
 use function Saeghe\Cli\IO\Read\parameter;
-use function Saeghe\FileManager\Resolver\root;
 
 function run(Project $project)
 {
     $seconds = (int) parameter('wait', 3);
 
-    $saeghe_path = root() . 'saeghe';
     chdir($project->root);
 
     while (true) {
-        echo shell_exec("php $saeghe_path build");
+        echo shell_exec("saeghe build");
 
         sleep($seconds);
     }
